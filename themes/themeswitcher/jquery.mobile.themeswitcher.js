45d2ef2d79e12b16ac674cc3c2fcee41a7889b0e
sgrebnov <sergei.grebnov@gmail.com>
2012-08-27 21:47:45 -0700

    fixes #9  Improve theme switcher

--- themes/themeswitcher/jquery.mobile.themeswitcher.js
+++ themes/themeswitcher/jquery.mobile.themeswitcher.js
@@ -66,8 +66,10 @@
         }
     };
 
-    //remover, adder
-    $.addTheme = function (themeId, updateCssLink) {
+    //remover, adder,
+    $.addTheme = function (themeId, updateCssLink, activePage) {
+
+       activePage = activePage || $.mobile.activePage;
 
         // TODO - potential issue - only particular styles should be removed
         if(updateCssLink)
@@ -96,13 +98,13 @@
 
         // based on http://stackoverflow.com/questions/8656801/how-to-change-theme-dynamically-in-jquery-mobile
 
-        $('.ui-btn').not('.ui-li-divider')
+        $(activePage).find('.ui-btn').not('.ui-li-divider')
             .removeClass('ui-btn-up-a ui-btn-up-b ui-btn-up-c ui-btn-up-d ui-btn-up-e ui-btn-hover-a ui-btn-hover-b ui-btn-hover-c ui-btn-hover-d ui-btn-hover-e')
             .addClass('ui-btn-up-' + theme)
             .attr('data-theme', theme);
 
         //target the list divider elements, then iterate through them to check if they have a theme set, if a theme is set then do nothing, otherwise change its theme to `b` (this is the jQuery Mobile default for list-dividers)
-        $('.ui-li-divider').each(function (index, obj) {
+        $(activePage).find('.ui-li-divider').each(function (index, obj) {
             if ($(this).parent().attr('data-divider-theme') == 'undefined') {
                 $(this).removeClass('ui-bar-a ui-bar-b ui-bar-c ui-bar-d ui-bar-e')
                     .addClass('ui-bar-b')
@@ -115,27 +117,31 @@
             .removeClass('ui-overlay-a ui-overlay-b ui-overlay-c ui-overlay-d ui-overlay-e')
             .addClass('ui-overlay-' + theme);
 
-	    $('.ui-slider-switch, .ui-slider')
+	    $(activePage).find('.ui-slider-switch, .ui-slider')
 		    .removeClass('ui-btn-down-a ui-btn-down-b ui-btn-down-c ui-btn-down-d ui-btn-down-e')
 		    .addClass('ui-btn-down-' + theme);
 
-        $('.ui-app-bar')
+        $(activePage).find('.ui-app-bar')
             .removeClass('ui-app-bar-a ui-app-bar-b')
             .addClass('ui-app-bar-' + theme);
 
         //reset the header/footer widgets
-        $('.ui-header, .ui-footer')
+        $(activePage).find('.ui-header, .ui-footer')
             .removeClass('ui-bar-a ui-bar-b ui-bar-c ui-bar-d ui-bar-e')
             .addClass('ui-bar-' + theme)
             .attr('data-theme', theme);
 
         //reset the page widget
-        $('.ui-page-active, .ui-content').removeClass('ui-body-a ui-body-b ui-body-c ui-body-d ui-body-e')
+        $(activePage).find('.ui-content').removeClass('ui-body-a ui-body-b ui-body-c ui-body-d ui-body-e')
+            .addClass('ui-body-' + theme)
+            .attr('data-theme', theme);
+
+	$(activePage).removeClass('ui-body-a ui-body-b ui-body-c ui-body-d ui-body-e')
             .addClass('ui-body-' + theme)
             .attr('data-theme', theme);
 
         // toggle button fix
-        $('.ui-toggle-button').removeClass('ui-toggle-button-a ui-toggle-button-b').addClass('ui-toggle-button-' + theme);
+        $(activePage).find('.ui-toggle-button').removeClass('ui-toggle-button-a ui-toggle-button-b').addClass('ui-toggle-button-' + theme);
 
         // update dialog overlay
         if ($.mobile.activePage) {
@@ -190,8 +196,8 @@
 
 	    $.addTheme(window.themeId, false);
 
-        $(document).bind('pageshow',function(event, ui){
-            $.addTheme(window.themeId, false);
+        $(document).bind('pagebeforeshow',function(event, ui){
+            $.addTheme(window.themeId, false, event.target);
         });
 
         var appendEl = $(this).find('#jqm-themeswitcher');
