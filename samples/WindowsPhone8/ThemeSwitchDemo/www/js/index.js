fba9638ef8fe76c31c6eec76a924b5eee6b1d7eb
sgrebnov <sergey.grebnov@akvelon.com>
2013-03-29 12:17:49 +0400

    fixes #22 Update demo apps to the latest Cordova version

--- samples/WindowsPhone8/ThemeSwitchDemo/www/js/index.js
+++ samples/WindowsPhone8/ThemeSwitchDemo/www/js/index.js
@@ -12,9 +12,5 @@ var app = {
     },
     report: function(id) { 
         console.log("report:" + id);
-        // hide the .pending <p> and show the .complete <p>
-        document.querySelector('#' + id + ' .pending').className += ' hide';
-        var completeElem = document.querySelector('#' + id + ' .complete');
-        completeElem.className = completeElem.className.split('hide').join('');
     }
 };
